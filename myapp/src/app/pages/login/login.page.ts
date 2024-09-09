0db1583266c07a8eb50c2b24a67cc8b68fb080bc
basto115 <bastianasd3@gmail.com>
2024-09-08 21:19:44 -0300

    weaitas de ts

--- myapp/src/app/pages/login/login.page.ts
+++ myapp/src/app/pages/login/login.page.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  user = {
+    username: '',
+    password: '',
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
+  
 
+  validar(){
+     if (this.user.username.length != 0) {
+      if (this.user.password.length != 0) {
+        console.log('Yippieee')
+        console.log('Datos validos')
+      }else{
+        console.log('Sin contraseña')
+
+      }
+     }else{
+        console.log('Sing usuario')
+     }
+  }
 }
