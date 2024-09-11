c45cf205ffdf5e4c54f337dfd80c7c543a753efd
basto115 <bastianasd3@gmail.com>
2024-09-11 17:39:57 -0300

    lol

--- myapp/src/app/pages/login/login.page.ts
+++ myapp/src/app/pages/login/login.page.ts
@@ -27,31 +27,32 @@ export class LoginPage implements OnInit {
   }
   
 
-  validar(){
-    if (this.user.username.length != 0) {
-     if (this.user.password.length != 0) {
-       console.log('Yippieee')
-       console.log('Datos validos')
-
-       let navigationExtras: NavigationExtras = {
-         state:{
-           user: this.user.username,
-           pass: this.user.password,
-         }
-       };
-       this.cambiarSpin();
-
-       setTimeout(() => {
-         this.router.navigate(['/inicio'], navigationExtras);
-         this.cambiarSpin();
-       }, 3500);
-
-     }else{
-       console.log('Sin contraseña')
-
-     }
-    }else{
-       console.log('Sin usuario')
+  validar() {
+    if (!this.user.username || this.user.username.length === 0) {
+      console.log('Sin usuario');
+      return;
     }
- }
+  
+    if (!this.user.password || this.user.password.length === 0) {
+      console.log('Sin contraseña');
+      return;
+    }
+  
+    console.log('Yippieee');
+    console.log('Datos válidos');
+  
+    let navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user.username,
+        pass: this.user.password,
+      }
+    };
+  
+    this.cambiarSpin();
+  
+    setTimeout(() => {
+      this.router.navigate(['/inicio'], navigationExtras);
+      this.cambiarSpin();
+    }, 3500);
+  }
 }
