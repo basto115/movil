618d741eb51ad737fbee6d4fd8afc4a07d55ea9f
basto115 <bastianasd3@gmail.com>
2024-10-23 22:12:31 -0300

    no se que hice, me pitie una wea

--- myapp/src/app/admin/controller/controller.page.ts
+++ myapp/src/app/admin/controller/controller.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APIControllerService } from 'src/app/servicios/apicontroller.service';
 
 @Component({
   selector: 'app-controller',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ControllerPage implements OnInit {
 
-  constructor() { }
+  users: any[] = [];
+  constructor(private api: APIControllerService) { }
 
   ngOnInit() {
+    this.cargarUsuarios();
   }
+  
+  cargarUsuarios() {
+    this.api.getUsers().subscribe(
+      (data) => {
+        this.users = data
+        console.log(this.users)
+      },
+      (error) => {
+        console.log("Error en la llamada :" + error)
+      });
+  }
+  modificarUsuario(id: any) {
 
+  }
+  eliminarUsuario(id: any) {
+
+  }
 }
