b250cc67bc80d460794b5cb267d8f10472cbe62b
jotakaii <jose.duran@duocuc.cl>
2024-11-25 17:42:27 -0300

    pipipi

--- myapp/src/app/pages/inicio/inicio.page.ts
+++ myapp/src/app/pages/inicio/inicio.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { QrCodeService } from '../services/qr-code.service'; // Asegúrate de importar el servicio
 
 @Component({
   selector: 'app-inicio',
@@ -8,13 +7,13 @@ import { QrCodeService } from '../services/qr-code.service'; // Asegúrate de im
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-  user: string | undefined;
-  qrCodeData: string = ''; // Texto que el usuario quiere convertir
-  qrCodeResult: string = ''; // Resultado del código QR
 
-  constructor(private router: Router, private qrCodeService: QrCodeService) { }
+  user: string | undefined;
+  
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    
     const navigation = this.router.getCurrentNavigation();
     const state = navigation?.extras?.state;
 
@@ -22,22 +21,5 @@ export class InicioPage implements OnInit {
       this.user = state['user'];  
     }
   }
-
-  generateQRCode() {
-    if (this.qrCodeData.trim() === '') {
-      alert('Por favor, ingresa un texto para generar el código QR.');
-      return;
-    }
-
-    this.qrCodeService.generateQRCode(this.qrCodeData).subscribe(
-      (response) => {
-        console.log('Código QR generado:', response);
-        this.qrCodeResult = response.qr_code; // Ajusta si el formato de respuesta varía
-      },
-      (error) => {
-        console.error('Error al generar el código QR:', error);
-        alert('Hubo un error al generar el código QR.');
-      }
-    );
-  }
+ 
 }
