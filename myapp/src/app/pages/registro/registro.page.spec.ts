86f8f34aecfa01ada6a12efffa9d494328373461
basto115 <bastianasd3@gmail.com>
2024-11-29 00:41:29 -0300

    Registro

--- myapp/src/app/pages/registro/registro.page.spec.ts
+++ myapp/src/app/pages/registro/registro.page.spec.ts
@@ -1,11 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegistroPage } from './registro.page';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { AuthenticatorService } from 'src/app/servicios/authenticator.service';
+import { FormsModule } from '@angular/forms';
 
 describe('RegistroPage', () => {
   let component: RegistroPage;
   let fixture: ComponentFixture<RegistroPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RegistroPage],
+      imports: [IonicStorageModule.forRoot(), FormsModule],
+      providers: [AuthenticatorService]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegistroPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
