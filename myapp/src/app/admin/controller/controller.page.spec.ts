12bfe11f689f6c16790331ba0aa409354c48407d
jotakaii <jose.duran@duocuc.cl>
2024-11-29 00:53:53 -0300

    controllerpage

--- myapp/src/app/admin/controller/controller.page.spec.ts
+++ myapp/src/app/admin/controller/controller.page.spec.ts
@@ -2,8 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ControllerPage } from './controller.page';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { APIControllerService } from 'src/app/servicios/apicontroller.service';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { IonicModule } from '@ionic/angular';
 
-
+// Mock para SQLite
+class SQLiteMock {
+  create() {
+    return Promise.resolve(); 
+  }
+}
 
 describe('ControllerPage', () => {
   let component: ControllerPage;
@@ -12,9 +19,12 @@ describe('ControllerPage', () => {
   beforeEach( async () => {
    await TestBed.configureTestingModule({
       declarations: [ControllerPage],
-      imports: [HttpClientTestingModule],
-      providers: [APIControllerService],
-    }).compileComponents
+      imports: [HttpClientTestingModule, IonicModule.forRoot()],
+      providers: [
+        APIControllerService,
+        { provide: SQLite, useClass: SQLiteMock } 
+      ],
+    }).compileComponents(); 
 
     const fixture = TestBed.createComponent(ControllerPage);
     component = fixture.componentInstance;
