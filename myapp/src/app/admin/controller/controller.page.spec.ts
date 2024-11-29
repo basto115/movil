0b6a0c1cc442307ccd12085d5a7386211e593be3
basto115 <bastianasd3@gmail.com>
2024-11-29 00:03:50 -0300

    apicontroller

--- myapp/src/app/admin/controller/controller.page.spec.ts
+++ myapp/src/app/admin/controller/controller.page.spec.ts
@@ -1,12 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ControllerPage } from './controller.page';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { APIControllerService } from 'src/app/servicios/apicontroller.service';
+
+
 
 describe('ControllerPage', () => {
   let component: ControllerPage;
   let fixture: ComponentFixture<ControllerPage>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ControllerPage);
+  beforeEach( async () => {
+   await TestBed.configureTestingModule({
+      declarations: [ControllerPage],
+      imports: [HttpClientTestingModule],
+      providers: [APIControllerService],
+    }).compileComponents
+
+    const fixture = TestBed.createComponent(ControllerPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- myapp/src/app/servicios/apicontroller.service.spec.ts
+++ myapp/src/app/servicios/apicontroller.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { APIControllerService } from './apicontroller.service';
 
 describe('APIControllerService', () => {
   let service: APIControllerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [APIControllerService],
+    });
     service = TestBed.inject(APIControllerService);
   });
 
