835c699e6006df59e22c63faa7a9c5f6c5d36e91
jotakaii <jose.duran@duocuc.cl>
2024-11-29 00:38:12 -0300

    appComponent

--- myapp/src/app/app.component.spec.ts
+++ myapp/src/app/app.component.spec.ts
@@ -1,7 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 
 import { AppComponent } from './app.component';
+
+// Mock para SQLite
+class SQLiteMock {
+  create() {
+    return Promise.resolve(); // Simula la creación de la base de datos
+  }
+}
 
 describe('AppComponent', () => {
 
@@ -9,6 +17,9 @@ describe('AppComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {provide: SQLite, useClass: SQLiteMock } // Provee el mock de SQLite
+      ],
     }).compileComponents();
   });
 
